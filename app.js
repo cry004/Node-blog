e1fc07714f5c97394354e4ce73df38a057411ea3
cry004 <cry004@gmail.com>
2018-10-04 18:16:44 +0800

    make template

--- app.js
+++ app.js
@@ -11,11 +11,14 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
+app.engine('ejs', require('express-ejs-extend')); // add this line
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,36 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'Express'
+  });
+});
+
+router.get('/post', function(req, res, next) {
+  res.render('post', {
+    title: 'Express'
+  });
+});
+
+router.get('/dashboard/archives', function(req, res, next) {
+  res.render('dashboard/archives', {
+    title: 'Express'
+  });
+});
+router.get('/dashboard/article', function(req, res, next) {
+  res.render('dashboard/article', {
+    title: 'Express'
+  });
+});
+router.get('/dashboard/categories', function(req, res, next) {
+  res.render('dashboard/categories', {
+    title: 'Express'
+  });
+});
+router.get('/dashboard/signup', function(req, res, next) {
+  res.render('dashboard/signup', {
+    title: 'Express'
+  });
 });
 
 module.exports = router;
